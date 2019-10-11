db8b8359af7e532993e8e0d58b860d9901573732
Georg Ahnert <mail@geahnert.de>
2019-10-11 11:47:16 +0200

    Add SOG blue to tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    colors: {
+      sogblue: {
+        lighter: '#71C5EA',
+        light: '#57B4DC',
+        default: '#0098CF',
+        dark: '#007EBB',
+        darker: '#00679E'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
