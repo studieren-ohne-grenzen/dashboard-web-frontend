6ef7878c0d8fae57ca276e32215d6619133b2c6e
Georg Ahnert <mail@geahnert.de>
2019-10-13 12:49:58 +0200

    Add dynamic routing for group names

---
+++ pages/index/groups/_group.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Untergruppe!
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.group)
+  }
+}
+</script>
+
+<style></style>
