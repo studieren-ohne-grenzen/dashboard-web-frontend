0e4016a2548b6f9ac5bfccc481d3817f3b346900
Georg Ahnert <mail@geahnert.de>
2019-10-23 17:22:53 +0200

    Add password request page

---
+++ pages/request-password.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bggradient w-screen min-h-screen h-full pt-4 lg:pt-32">
+    <img alt="Dashboard Logo" src="logo.png" class="mx-auto py-8 px-2 w-64" />
+    <div class="bg-white max-w-md bg-white xs:mx-auto xs:rounded p-8">
+      <div v-if="$auth.$state.redirect">Login first!</div>
+      <form @keydown.enter="login">
+        <label class="block text-sogblue-dark mb-1">Alternative Mail</label>
+        <input
+          ref="username"
+          v-model="username"
+          placeholder="maria.schmidt@gmail.com"
+          class="p-2 mb-4 w-full rounded appearance-none bg-lightgray text-sogblue-darker focus:shadow-outline focus:bg-white"
+        />
+        <div class="text-gray mb-4">
+          Wir senden dir an deine alternative Mail-Adresse einen Link zum
+          Zurücksetzen des Passworts.
+        </div>
+        <button
+          class="xs:float-left rounded py-2 px-4 bg-sogblue hover:bg-sogblue-darker text-white"
+        >
+          Zurücksetzen
+        </button>
+        <div class="w-full mt-8 xs:mt-0 mb-12">
+          <nuxt-link
+            to="/"
+            class="xs:float-right rounded py-2 px-4 bg-white border border-sogblue hover:bg-sogblue-light text-sogblue hover:text-white"
+          >
+            Abbrechen
+          </nuxt-link>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  options: {
+    auth: false
+  }
+}
+</script>
+
+<style></style>
