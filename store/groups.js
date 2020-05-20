6c79bc721d1f1bc61028d4d3168f7070479ea1cb
Georg Ahnert <mail@geahnert.de>
2020-05-20 16:19:25 +0200

    Shorten groups getters

--- store/groups.js
+++ store/groups.js
@@ -1,36 +1,43 @@
 export const state = () => ({
   groupList: [
     {
+      id: 1,
       name: 'AG Design',
       membership: 'member',
       groupType: 'bundesweit',
     },
     {
+      id: 2,
       name: 'LG Aachen',
       membership: 'member',
       groupType: 'lokalgruppe',
     },
     {
+      id: 3,
       name: 'AG Kongo - Mweso Stipendienprogramm',
       membership: '',
       groupType: 'projektgruppe',
     },
     {
+      id: 4,
       name: 'LG Konstanz',
       membership: 'admin',
       groupType: 'lokalgruppe',
     },
     {
+      id: 5,
       name: 'Ressort IT',
       membership: 'pending',
       groupType: 'bundesweit',
     },
     {
+      id: 6,
       name: 'AG Öffentlichkeitsarbeit',
       membership: '',
       groupType: 'bundesweit',
     },
     {
+      id: 7,
       name: 'AG Ruanda',
       membership: '',
       groupType: 'projektgruppe',
@@ -48,62 +55,58 @@ const compareGroups = (a, b) => {
 }
 
 export const getters = {
-  allGroupsLokal(state) {
-    return [
+  allGroupsLokal: (state) =>
+    [
       ...state.groupList.filter((group) => group.groupType === 'lokalgruppe'),
-    ].sort(compareGroups)
-  },
-  allGroupsProjekt(state) {
-    return [
+    ].sort(compareGroups),
+
+  allGroupsProjekt: (state) =>
+    [
       ...state.groupList.filter((group) => group.groupType === 'projektgruppe'),
-    ].sort(compareGroups)
-  },
-  allGroupsBund(state) {
-    return [
+    ].sort(compareGroups),
+
+  allGroupsBund: (state) =>
+    [
       ...state.groupList.filter((group) => group.groupType === 'bundesweit'),
-    ].sort(compareGroups)
-  },
-  allGroupsOther(state) {
-    return [
-      ...state.groupList.filter((group) => group.groupType === 'other'),
-    ].sort(compareGroups)
-  },
-  allGroupsByCategory(_, getters) {
-    return [
-      {
-        name: 'Bundesweite Gruppen',
-        getter: getters.allGroupsBund,
-      },
-      {
-        name: 'Projektgruppen',
-        getter: getters.allGroupsProjekt,
-      },
-      {
-        name: 'Lokalgruppen',
-        getter: getters.allGroupsLokal,
-      },
-      {
-        name: 'Andere Gruppen',
-        getter: getters.allGroupsOther,
-      },
-    ]
-  },
-  allGroups(state) {
-    return [...state.groupList].sort(compareGroups)
-  },
-  myGroups(state) {
-    return [...state.groupList.filter((group) => group.membership !== '')].sort(
+    ].sort(compareGroups),
+
+  allGroupsOther: (state) =>
+    [...state.groupList.filter((group) => group.groupType === 'other')].sort(
       compareGroups
-    )
-  },
-  adminGroups(state) {
-    return [
-      ...state.groupList.filter((group) => group.membership === 'admin'),
-    ].sort(compareGroups)
-  },
-  requests(state) {
-    return state.requests
-  },
+    ),
+
+  allGroupsByCategory: (_, getters) => [
+    {
+      name: 'Bundesweite Gruppen',
+      getter: getters.allGroupsBund,
+    },
+    {
+      name: 'Projektgruppen',
+      getter: getters.allGroupsProjekt,
+    },
+    {
+      name: 'Lokalgruppen',
+      getter: getters.allGroupsLokal,
+    },
+    {
+      name: 'Andere Gruppen',
+      getter: getters.allGroupsOther,
+    },
+  ],
+
+  allGroups: (state) => [...state.groupList].sort(compareGroups),
+
+  myGroups: (state) =>
+    [...state.groupList.filter((group) => group.membership !== '')].sort(
+      compareGroups
+    ),
+
+  adminGroups: (state) =>
+    [...state.groupList.filter((group) => group.membership === 'admin')].sort(
+      compareGroups
+    ),
+
+  requests: (state) => state.requests,
 }
 
 export const actions = {
