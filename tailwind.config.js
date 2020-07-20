b1846530fc82d59329277011dccd167946ad3328
Georg Ahnert <mail@geahnert.de>
2020-07-20 11:49:11 +0200

    re-enable purging css

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,4 +40,18 @@ module.exports = {
   },
   variants: {},
   plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    // enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+    options: {
+      whitelist: ['bggradient'],
+    },
+  },
 }
