875c73cd9bc48d84ffacc8c431ca14943b17ba88
Laurenz Grote <laurenzfg@laurenzgrote.de>
2020-08-09 14:08:00 +0200

    Use staging server

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,7 +101,7 @@ export default {
    */
   proxy: {
     '/api': {
-      target: 'http://localhost:5000', // Login Server goes here
+      target: 'https://vogelnest.de-staging.esf-international.org/', // Login Server goes here
       pathRewrite: {
         '^/api': '/',
       },
