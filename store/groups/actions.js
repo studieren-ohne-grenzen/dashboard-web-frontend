cff4381f7bef33f9069246e954f3616d97b4ee64
Georg Ahnert <mail@geahnert.de>
2020-08-09 15:34:25 +0200

    Wire up store actions for groups

--- store/groups/actions.js
+++ store/groups/actions.js
@@ -67,4 +67,180 @@ export default {
     // API request to be implemented here
     commit('setMType', { groupID, mType: '' })
   },
+
+  alertAddGuest({ commit, getters }, { name, email, groupID }) {
+    const message =
+      'Möchtest du ' +
+      name +
+      ' wirklich zu ' +
+      getters.allGroups.find((group) => group.id === groupID).name +
+      ' hinzufügen? Sie/er ist dann auf der Mailingliste der Gruppe eingetragen. ' +
+      'Für Zugriff auf weitere SOG-Tools (Wiki, Mattermost, etc.) muss sie/er aber reguläres Mitglied werden.'
+    commit(
+      'alertbox/showAlert',
+      {
+        title: 'Gast hinzufügen',
+        message,
+        defaultToAction: true,
+        actionName: 'Hinzufügen',
+        action: 'groups/addGuest',
+        params: { name, email, groupID },
+      },
+      { root: true }
+    )
+  },
+  addGuest({ commit }, { name, email, groupID }) {
+    // API request to be implemented here
+    commit('addGuest', { name, email, groupID })
+  },
+
+  alertRemoveGuest({ commit, getters }, { uid, groupID }) {
+    const message =
+      'Möchtest du ' +
+      getters.allGroups
+        .find((group) => group.id === groupID)
+        .guests.find((g) => g.uid === uid).name +
+      ' wirklich aus ' +
+      getters.allGroups.find((group) => group.id === groupID).name +
+      ' entfernen? Sie/er ist dann nicht mehr auf der Mailingliste der Gruppe eingetragen.'
+    commit(
+      'alertbox/showAlert',
+      {
+        title: 'Gast entfernen',
+        message,
+        defaultToAction: false,
+        actionName: 'Entfernen',
+        action: 'groups/removeGuest',
+        params: { uid, groupID },
+      },
+      { root: true }
+    )
+  },
+  removeGuest({ commit }, { uid, groupID }) {
+    // API request to be implemented here
+    commit('removeGuest', { uid, groupID })
+  },
+
+  alertRemoveMember({ commit, getters }, { uid, groupID }) {
+    const message =
+      'Möchtest du ' +
+      getters.allGroups
+        .find((group) => group.id === groupID)
+        .members.find((g) => g.uid === uid).name +
+      ' wirklich aus ' +
+      getters.allGroups.find((group) => group.id === groupID).name +
+      ' entfernen? Sie/er hat dann keinen Zugriff mehr auf das Mattermost-Team und den Nextcloud-Ordner' +
+      ' und bekommt keine Mails mehr über die Mailingliste.'
+    commit(
+      'alertbox/showAlert',
+      {
+        title: 'Mitglied entfernen',
+        message,
+        defaultToAction: false,
+        actionName: 'Entfernen',
+        action: 'groups/removeMember',
+        params: { uid, groupID },
+      },
+      { root: true }
+    )
+  },
+  removeMember({ commit }, { uid, groupID }) {
+    // API request to be implemented here
+    commit('removeMember', { uid, groupID })
+  },
+
+  addMember({ commit, rootGetters }, { uid, groupID }) {
+    // API request to be implemented here
+    const user = rootGetters['users/all'].find((u) => u.uid === uid)
+    commit('addMember', { user, groupID })
+  },
+
+  alertMakeAdmin({ commit, getters }, { uid, groupID }) {
+    const message =
+      'Möchtest du ' +
+      getters.allGroups
+        .find((group) => group.id === groupID)
+        .members.find((g) => g.uid === uid).name +
+      ' wirklich zur Koordinator:in von ' +
+      getters.allGroups.find((group) => group.id === groupID).name +
+      ' ernennen? Sie/er kann dann die Mitglieder der Gruppe bearbeiten' +
+      ' und weitere Mitglieder zu Koordinator:innen ernennen.'
+    commit(
+      'alertbox/showAlert',
+      {
+        title: 'Mitglied zur Koordinator:in befördern',
+        message,
+        defaultToAction: false,
+        actionName: 'Befördern',
+        action: 'groups/makeAdmin',
+        params: { uid, groupID },
+      },
+      { root: true }
+    )
+  },
+  makeAdmin({ commit, rootGetters }, { uid, groupID }) {
+    // API request to be implemented here
+    commit('removeMember', { uid, groupID })
+    const user = rootGetters['users/all'].find((u) => u.uid === uid)
+    commit('addAdmin', { user, groupID })
+  },
+
+  alertRevokeAdmin({ commit, getters }, { uid, groupID }) {
+    if (
+      getters.allGroups.find((group) => group.id === groupID).admins.length ===
+      1
+    ) {
+      const userName = getters.allGroups
+        .find((group) => group.id === groupID)
+        .admins.find((g) => g.uid === uid).name
+      const message =
+        userName +
+        ' ist die/der letzte Koordinator:in von ' +
+        getters.allGroups.find((group) => group.id === groupID).name +
+        '. Füge zunächst weitere Koordinator:innen hinzu, bevor du ' +
+        userName +
+        ' entfernst.'
+      commit(
+        'alertbox/showAlert',
+        {
+          title: 'Koordinator:innen-Rechte entziehen',
+          message,
+          defaultToAction: true,
+          actionName: 'OK',
+          showCancel: false,
+          action: '',
+          params: { uid, groupID },
+        },
+        { root: true }
+      )
+    } else {
+      const message =
+        'Möchtest du ' +
+        getters.allGroups
+          .find((group) => group.id === groupID)
+          .admins.find((g) => g.uid === uid).name +
+        ' die Koordinator:innen-Rechte für ' +
+        getters.allGroups.find((group) => group.id === groupID).name +
+        ' entziehen? Sie/er kann dann die Mitglieder der Gruppe nicht mehr bearbeiten' +
+        ' und keine weiteren Mitglieder zu Koordinator:innen ernennen.'
+      commit(
+        'alertbox/showAlert',
+        {
+          title: 'Koordinator:innen-Rechte entziehen',
+          message,
+          defaultToAction: false,
+          actionName: 'Entziehen',
+          action: 'groups/revokeAdmin',
+          params: { uid, groupID },
+        },
+        { root: true }
+      )
+    }
+  },
+  revokeAdmin({ commit, rootGetters }, { uid, groupID }) {
+    // API request to be implemented here
+    commit('removeAdmin', { uid, groupID })
+    const user = rootGetters['users/all'].find((u) => u.uid === uid)
+    commit('addMember', { user, groupID })
+  },
 }
--- store/groups/mutations.js
+++ store/groups/mutations.js
@@ -2,4 +2,39 @@ export default {
   setMType(state, { groupID, mType }) {
     state.groupList.find((group) => group.id === groupID).membership = mType
   },
+  addGuest(state, { name, email, groupID }) {
+    const guest = {
+      name,
+      email,
+      uid: 'guest' + name.toLowerCase().replace(/ /i, '.'),
+    }
+    state.groupList.find((group) => group.id === groupID).guests.push(guest)
+  },
+  removeGuest(state, { uid, groupID }) {
+    state.groupList.find(
+      (group) => group.id === groupID
+    ).guests = state.groupList
+      .find((group) => group.id === groupID)
+      .guests.filter((g) => g.uid !== uid)
+  },
+  removeMember(state, { uid, groupID }) {
+    state.groupList.find(
+      (group) => group.id === groupID
+    ).members = state.groupList
+      .find((group) => group.id === groupID)
+      .members.filter((g) => g.uid !== uid)
+  },
+  addMember(state, { user, groupID }) {
+    state.groupList.find((group) => group.id === groupID).members.push(user)
+  },
+  removeAdmin(state, { uid, groupID }) {
+    state.groupList.find(
+      (group) => group.id === groupID
+    ).admins = state.groupList
+      .find((group) => group.id === groupID)
+      .admins.filter((g) => g.uid !== uid)
+  },
+  addAdmin(state, { user, groupID }) {
+    state.groupList.find((group) => group.id === groupID).admins.push(user)
+  },
 }
