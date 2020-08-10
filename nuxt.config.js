86dd0f642619804ad275defd6ae3aaedfc4ad7e7
Georg Ahnert <mail@geahnert.de>
2020-08-10 09:37:23 +0200

    Persist some stuff in local storage

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~/plugins/localStorage.js', ssr: false }],
   /*
    ** Router options
    */
---
+++ plugins/localStorage.js
@@ -0,0 +1,15 @@
+import createPersistedState from 'vuex-persistedstate'
+
+export default ({ store }) => {
+  createPersistedState({
+    key: 'dashboard',
+    paths: [
+      'groups.groupList',
+      'users.users',
+      'user.name',
+      'user.username',
+      'user.sogMail',
+      'user.altMail',
+    ],
+  })(store)
+}
