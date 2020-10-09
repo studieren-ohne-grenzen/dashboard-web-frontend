5302772087e12d1c3d509b16723ceb4306078422
Georg Ahnert <mail@geahnert.de>
2020-10-09 12:18:14 +0200

    Fix background attachment

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,5 +6,6 @@
 html {
   background-color: #00679e;
   background-image: linear-gradient(20deg, #57b4dc 0%, #00679e 100%);
+  background-attachment: fixed;
 }
 </style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -32,5 +32,6 @@ export default {
 html {
   background-color: #00679e;
   background-image: linear-gradient(20deg, #57b4dc 0%, #00679e 100%);
+  background-attachment: fixed;
 }
 </style>
