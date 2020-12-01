315c9e3e6f590a558f5b71081c58dbce3a6499f7
Georg Ahnert <mail@geahnert.de>
2020-12-01 12:52:01 +0100

    Add dark versions of login & pwd reset

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="w-screen min-h-screen h-full pt-4 lg:pt-32">
-    <img alt="Dashboard Logo" src="logo.png" class="mx-auto py-8 px-2 w-64" />
-    <div class="bg-white max-w-md bg-white xs:mx-auto xs:rounded p-8">
+    <img
+      alt="Dashboard Logo"
+      src="logo.png"
+      class="mx-auto py-8 px-2 w-64 dark:opacity-80"
+    />
+    <div class="bg-white max-w-md dark:bg-gray-900 xs:mx-auto xs:rounded p-8">
       <div
         v-if="wrongLogin"
-        class="block p-2 mb-8 rounded border border-red-600 text-red-600"
+        class="block p-2 mb-8 rounded border border-red-600 text-red-600 dark:bg-gray-800"
       >
         Falsche Logindaten
       </div>
@@ -15,29 +19,33 @@
         Fehler bei der Kommunikation mit dem Server
       </div>
       <form @submit.prevent="login">
-        <label class="block text-sogblue-dark mb-1">Benutzername</label>
+        <label class="block text-sogblue-dark dark:text-gray-300 mb-1">
+          Benutzername
+        </label>
         <input
           ref="username"
           v-model="username"
           placeholder="vorname.nachname"
-          class="p-2 mb-4 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:shadow-outline focus:bg-white"
+          class="p-2 mb-4 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:ring-2 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 dark:text-white dark:focus:ring-gray-500"
         />
-        <label class="block text-sogblue-dark mb-1">Passwort</label>
+        <label class="block text-sogblue-dark mb-1 dark:text-gray-300">
+          Passwort
+        </label>
         <input
           v-model="password"
           type="password"
           placeholder="********"
-          class="p-2 mb-8 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:shadow-outline focus:bg-white"
+          class="p-2 mb-8 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:ring-2 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 dark:text-white dark:focus:ring-gray-500"
         />
         <button
-          class="xs:float-left rounded py-2 px-4 bg-sogblue hover:bg-sogblue-darker text-white"
+          class="xs:float-left rounded py-2 px-4 bg-sogblue hover:bg-sogblue-darker text-white dark:bg-sogblue dark:hover:bg-sogblue-light dark:text-black"
         >
           Login
         </button>
         <div class="w-full mt-8 xs:mt-0 mb-8">
           <nuxt-link
             to="/request-password"
-            class="xs:float-right rounded py-2 px-4 bg-white border border-sogblue hover:bg-sogblue-light text-sogblue hover:text-white"
+            class="xs:float-right rounded py-2 px-4 bg-white border border-sogblue hover:bg-sogblue-light text-sogblue hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-300 dark:border-gray-900"
           >
             Passwort vergessen
           </nuxt-link>
@@ -60,6 +68,7 @@ export default {
   },
   options: {
     auth: 'guest',
+    layout: 'dark',
   },
   computed: {
     redirect() {
--- pages/request-password.vue
+++ pages/request-password.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="w-screen min-h-screen h-full pt-4 lg:pt-32">
-    <img alt="Dashboard Logo" src="logo.png" class="mx-auto py-8 px-2 w-64" />
-    <div class="bg-white max-w-md bg-white xs:mx-auto xs:rounded p-8">
+    <img
+      alt="Dashboard Logo"
+      src="logo.png"
+      class="mx-auto py-8 px-2 w-64 dark:opacity-80"
+    />
+    <div
+      class="bg-white max-w-md bg-white dark:bg-gray-900 xs:mx-auto xs:rounded p-8"
+    >
       <form @submit.prevent="sendRequest">
-        <label class="block text-sogblue-dark mb-1">Alternative Mail</label>
+        <label class="block text-sogblue-dark mb-1 dark:text-gray-300">
+          Alternative Mail
+        </label>
         <input
           ref="mail"
           v-model="mail"
           v-focus
           :class="emailError ? 'border-red-500 border-2' : 'border-none'"
-          class="p-2 mb-4 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:shadow-outline focus:bg-white"
+          class="p-2 mb-4 w-full rounded appearance-none bg-gray-light text-sogblue-darker focus:ring-2 focus:bg-white dark:bg-gray-800 dark:focus:bg-gray-700 dark:text-white dark:focus:ring-gray-500"
         />
         <div v-if="emailError" class="flex-grow w-full text-red-600 mb-4">
           Keine gültige Mailadresse eingegeben.
@@ -18,17 +26,17 @@
           :disabled="!changeEmailSubmittable"
           :class="
             changeEmailSubmittable
-              ? 'cursor-pointer bg-sogblue hover:bg-sogblue-darker'
-              : 'cursor-default bg-sogblue-lightest hover:bg-sogblue-lightest'
+              ? 'cursor-pointer bg-sogblue hover:bg-sogblue-darker dark:bg-sogblue dark:hover:bg-sogblue-light'
+              : 'cursor-default bg-sogblue-lightest hover:bg-sogblue-lightest dark:bg-gray-800 dark:hover:bg-gray-800'
           "
-          class="xs:float-left rounded py-2 px-4 text-white"
+          class="xs:float-left rounded py-2 px-4 text-white dark:text-black"
         >
           Zurücksetzen
         </button>
         <div class="w-full mt-8 xs:mt-0 mb-12">
           <nuxt-link
             to="/login"
-            class="xs:float-right rounded py-2 px-4 bg-white border border-sogblue hover:bg-sogblue-light text-sogblue hover:text-white"
+            class="xs:float-right rounded py-2 px-4 bg-white border border-sogblue hover:bg-sogblue-light text-sogblue hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-300 dark:border-gray-900"
           >
             Abbrechen
           </nuxt-link>
@@ -63,6 +71,7 @@ export default {
   },
   options: {
     auth: 'guest',
+    layout: 'dark',
   },
   watch: {
     mail() {
